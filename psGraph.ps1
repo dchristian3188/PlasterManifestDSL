34d46396381acf56cb46bf40ba4889f28c1c7934
dchristian3188 <dchristian3188@gmail.com>
2017-06-14 07:47:59 -0700

    Added PSGraph

---
+++ psGraph.ps1
@@ -0,0 +1,27 @@
+graph Monkey {
+    $moduleRoot = $PSScriptRoot
+    $scripts = @{}
+    Get-ChildItem $moduleRoot -Filter *.ps1 -Recurse -File |
+        Where-Object -FilterScript {$PSItem.FullName -notmatch 'Examples|tests'} |
+        ForEach-Object -Process {
+            $scripts[$PSItem.BaseName] = $PSItem.FullName
+    }
+
+    $scriptNames = $scripts.GetEnumerator()  | Select-Object -ExpandProperty Key
+    ForEach ($script in $scriptNames)
+    {
+
+        node $script
+        $contents = Get-Content -Path $scripts[$script] -ErrorAction Stop
+        $errors = $null
+        $commands = ([System.Management.Automation.PSParser]::Tokenize($contents, [ref]$errors) |
+                Where-Object -FilterScript {$PSItem.Type -eq 'Command'}).Content
+        ForEach ($command in $commands)
+        {
+            If ($scripts[$command])
+            {
+               Edge  $script -To $command
+            }
+        }
+    }
+} | Export-PSGraph -ShowGraph
