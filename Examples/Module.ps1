32139d4867ea0f477f91a9147033d279a973fd56
dchristian3188 <dchristian3188@gmail.com>
2017-08-22 22:45:13 -0700

    Updated Module Example

--- Examples/Module.ps1
+++ Examples/Module.ps1
@@ -20,7 +20,7 @@ PlasterManifest {
             Choice -Label 'Internal' -Help 'Adds a internal folder to module root'
             Choice -Label 'Classes' -Help "Adds a classes folder to module root"
             Choice -Label 'Binaries' -Help 'Adds a binaries folder to module root'
-            Choice -Label 'Data' -Help 'Adds a data folder to module root'
+            Choice -Label 'DSCResources' -Help 'Adds a DSCResource folder to module root'
         }
 
         SingleChoice -Name "Pester" -Prompt "Include Pester Tests?" -Default 0 {
@@ -38,83 +38,96 @@ PlasterManifest {
             Choice -Label "No" -Help "Does not include any invoke build scripts"
         }
 
-        SingleChoice -Name "Graphiz" -Prompt "Include Grphiz" -Default 0 {
-            Choice -Label "Yes" -Help "Adds a graphiz diagram"
-            Choice -Label "No" -Help "Does not add graphiz support"
+        SingleChoice -Name "PSGraph" -Prompt "Include PSGraph" -Default 0 {
+            Choice -Label "Yes" -Help "Adds a PSGraph diagram"
+            Choice -Label "No" -Help "Does not add PSGraph support"
+        }
+
+        SingleChoice -Name "PlatyPS" -Prompt "Include PlatyPS" -Default 0 {
+            Choice -Label "Yes" -Help "Adds a PlatyPS for help"
+            Choice -Label "No" -Help "Does not add PlatyPS support"
         }
     }
 
     Content {
         NewModuleManifest {
-            Destination = '${PLASTER_PARAM_ModuleName}.psd1'
+            Destination = '${PLASTER_PARAM_ModuleName}\${PLASTER_PARAM_ModuleName}.psd1'
             ModuleVersion = '$PLASTER_PARAM_ModuleVersion'
             RootModule = '${PLASTER_PARAM_ModuleName}.psm1'
             Author = '$PLASTER_PARAM_ModuleAuthor'
             description = '$PLASTER_PARAM_ModuleDesc'
         }
 
-        TemplateFile -Source 'template.psm1' -Destination '${PLASTER_PARAM_ModuleName}.psm1'
+        TemplateFile -Source 'template.psm1' -Destination '${PLASTER_PARAM_ModuleName}\${PLASTER_PARAM_ModuleName}.psm1'
 
         Message "Creating you folders for module: $PLASTER_PARAM_ModuleName"
 
-        File -Source '' -Destination 'Public\' {
+        File -Source '' -Destination '${PLASTER_PARAM_ModuleName}\Public\' {
             $PLASTER_PARAM_FunctionFolders -contains 'Public'
         }
 
-        File -Source '' -Destination 'Internal\' {
+        File -Source '' -Destination '${PLASTER_PARAM_ModuleName}\Internal\' {
             $PLASTER_PARAM_FunctionFolders -contains 'Internal'
         }
 
-        File -Source '' -Destination 'Classes\' {
+        File -Source '' -Destination '${PLASTER_PARAM_ModuleName}\Classes\' {
             $PLASTER_PARAM_FunctionFolders -contains 'Classes'
         }
 
-        File -Source '' -Destination 'Binaries\' {
+        File -Source '' -Destination '${PLASTER_PARAM_ModuleName}\Binaries\' {
             $PLASTER_PARAM_FunctionFolders -contains 'Binaries'
         }
 
-        File -Source '' -Destination 'Data\' {
-            $PLASTER_PARAM_FunctionFolders -contains 'Data'
-        }
-
-        Message "Creating a Tests folder" {
-            $PLASTER_PARAM_Pester -eq "Yes"
+        File -Source '' -Destination '${PLASTER_PARAM_ModuleName}\DSCResources\' {
+            $PLASTER_PARAM_FunctionFolders -contains 'DSCResources'
         }
 
         RequireModule -Name "Pester" -MinimumVersion '3.4.0' {
             $PLASTER_PARAM_Pester -eq "Yes"
         }
 
-        File -Source '' -Destination 'Tests\' {
+        Message "Creating a Tests folder" {
             $PLASTER_PARAM_Pester -eq "Yes"
         }
 
-        File -Source 'basicTest.ps1' -Destination 'Tests\${PLASTER_PARAM_ModuleName}.tests.ps1' {
+        File -Source '' -Destination '${PLASTER_PARAM_ModuleName}\Tests\' {
             $PLASTER_PARAM_Pester -eq "Yes"
         }
 
-        File -Source '.gitignore' -Destination '.gitignore' {
+        File -Source 'basicTest.ps1' -Destination '${PLASTER_PARAM_ModuleName}\Tests\${PLASTER_PARAM_ModuleName}.tests.ps1' {
+            $PLASTER_PARAM_Pester -eq "Yes"
+        }
+
+        File -Source '.gitignore' -Destination '${PLASTER_PARAM_ModuleName}\.gitignore' {
             $PlASTER_PARAM_Git -eq "Yes"
-        }
-
-        TemplateFile -Source 'default.build.ps1' -Destination '${PLASTER_PARAM_ModuleName}.build.ps1' {
-            $PLASTER_PARAM_InvokeBuild -eq "Yes"
         }
 
         RequireModule -Name 'InvokeBuild' {
             $PLASTER_PARAM_InvokeBuild -eq 'Yes'
         }
 
+        TemplateFile -Source 'default.build.ps1' -Destination '${PLASTER_PARAM_ModuleName}\${PLASTER_PARAM_ModuleName}.build.ps1' {
+            $PLASTER_PARAM_InvokeBuild -eq "Yes"
+        }
+
         RequireModule -Name 'PSGraph' {
-            $PLASTER_PARAM_Graphiz -eq 'Yes'
+            $PLASTER_PARAM_PSGraph -eq 'Yes'
         }
 
-        File -Source '' -Destination 'Graphs' {
-            $PLASTER_PARAM_Graphiz -eq 'Yes'
+        File -Source '' -Destination '${PLASTER_PARAM_ModuleName}\Graphs' {
+            $PLASTER_PARAM_PSGraph -eq 'Yes'
         }
 
-        File -Source 'CommandFlow.ps1' -Destination 'Graphs\CommandFlow.ps1' {
-            $PLASTER_PARAM_Graphiz -eq 'Yes'
+        File -Source 'CommandFlow.ps1' -Destination '${PLASTER_PARAM_ModuleName}\Graphs\CommandFlow.ps1' {
+            $PLASTER_PARAM_PSGraph -eq 'Yes'
+        }
+
+        RequireModule -Name 'PlatyPS' {
+            $PLASTER_PARAM_PlatyPS -eq 'Yes'
+        }
+
+        File -Source '' -Destination '${PLASTER_PARAM_ModuleName}\Help' {
+            $PLASTER_PARAM_PlatyPS -eq 'Yes'
         }
     }
 }  |  Export-PlasterManifest -Destination C:\temp\plasterManifest.xml -Verbose -PassThru |
